426863c9235f7a65f1d0b06d5d0c1a2f07e308f9
wingxy <wingxy19891017@gmail.com>
2018-05-28 21:19:37 +0800

    支付成功回调页状态显示

--- meiduo_mall/meiduo_mall/apps/payment/views.py
+++ meiduo_mall/meiduo_mall/apps/payment/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from orders.models import OrderInfo
+from payment.models import Payment
 
 
 class PaymentView(APIView):
@@ -28,6 +29,7 @@ class PaymentView(APIView):
         # 根据订单的数据，向支付宝发起请求，获取支付链接参数
         alipay_client = AliPay(
             appid=settings.ALIPAY_APPID,
+            # TODO: 没有集成异步接受支付宝通知
             app_notify_url=None,  # 默认回调url
             app_private_key_path=os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                               "keys/app_private_key.pem"),
@@ -46,3 +48,43 @@ class PaymentView(APIView):
         alipay_url = settings.ALIPAY_GATEWAY_URL + '?' + order_string
 
         return Response({'alipay_url': alipay_url})
+
+
+class PaymentStatusView(APIView):
+    """
+    修改支付结果状态
+    """
+
+    def put(self, request):
+
+        query_dict = request.query_params
+        # 将django中的QueryDict 转换python的字典
+        alipay_data_dict = query_dict.dict()
+        sign = alipay_data_dict.pop('sign')
+
+
+        alipay_client = AliPay(
+            appid=settings.ALIPAY_APPID,
+            app_notify_url=None,  # 默认回调url
+            app_private_key_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "keys/app_private_key.pem"),
+            alipay_public_key_path=os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                                "keys/alipay_public_key.pem"),  # 支付宝的公钥，验证支付宝回传消息使用，不是你自己的公钥,
+            sign_type="RSA2",  # RSA 或者 RSA2
+            debug=settings.ALIPAY_DEBUG  # 默认False
+        )
+
+        success = alipay_client.verify(alipay_data_dict, sign)
+
+        if success:
+            order_id = alipay_data_dict.get('out_trade_no')
+            trade_id = alipay_data_dict.get('trade_no')  # 支付宝交易流水号
+            Payment.objects.create(
+                order_id=order_id,
+                trade_id=trade_id
+            )
+            OrderInfo.objects.filter(order_id=order_id, status=OrderInfo.ORDER_STATUS_ENUM['UNPAID']).update(
+                status=OrderInfo.ORDER_STATUS_ENUM['UNSEND'])
+            return Response({'trade_id': trade_id})
+        else:
+            # 参数据不是支付宝的，是非法请求
+            return Response({'message': '非法请求'}, status=status.HTTP_403_FORBIDDEN)
